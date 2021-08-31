bf97ed421775944c3bbde705a56b1adf623cb758
ycszen <changqian_yu@163.com>
2021-09-01 00:05:14 +0800

    first commit

--- mmseg/models/decode_heads/__init__.py
+++ mmseg/models/decode_heads/__init__.py
@@ -24,11 +24,14 @@ from .sep_fcn_head import DepthwiseSeparableFCNHead
 from .setr_mla_head import SETRMLAHead
 from .setr_up_head import SETRUPHead
 from .uper_head import UPerHead
+from .cond_aspp_head import CondASPPHead
+from .cond_sep_aspp_head import CondDepthwiseSeparableASPPHead
 
 __all__ = [
     'FCNHead', 'PSPHead', 'ASPPHead', 'PSAHead', 'NLHead', 'GCHead', 'CCHead',
     'UPerHead', 'DepthwiseSeparableASPPHead', 'ANNHead', 'DAHead', 'OCRHead',
     'EncHead', 'DepthwiseSeparableFCNHead', 'FPNHead', 'EMAHead', 'DNLHead',
-    'PointHead', 'APCHead', 'DMHead', 'LRASPPHead', 'SETRUPHead',
+    'PointHead', 'APCHead', 'DMHead', 'LRASPPHead', 'CondASPPHead',
+    'CondDepthwiseSeparableASPPHead', 'SETRUPHead',
     'SETRMLAHead', 'DPTHead', 'SETRMLAHead', 'SegformerHead'
 ]
