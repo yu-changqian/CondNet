2d986d20c2fffd5dd196f3521e284331dd513ad5
Yu Changqian <yuchangqian@YudeMacBook-Pro.local>
2021-09-22 15:13:15 +0800

    [Clear]clear remote files

--- configs/condnet/condnet_r50-d8_headlrx10_512x512_160k_ade20k.py
+++
@@ -1,7 +0,0 @@
-_base_ = [
-    '../_base_/models/condnet_r50-d8.py', '../_base_/datasets/ade20k.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
-]
-model = dict(
-    decode_head=dict(num_classes=150), auxiliary_head=dict(num_classes=150))
-optimizer = dict(paramwise_cfg=dict(custom_keys={'head': dict(lr_mult=10.)}))
