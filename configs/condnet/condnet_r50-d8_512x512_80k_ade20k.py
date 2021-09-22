19483726d675d5bd3d4f5aa6773a28c17b8ba2ab
Yu Changqian <yuchangqian@YudeMacBook-Pro.local>
2021-09-22 15:16:13 +0800

    [Feature]support R50

--- configs/condnet/condnet_r50-d8_512x512_80k_ade20k.py
+++ configs/condnet/condnet_r50-d8_512x512_80k_ade20k.py
@@ -2,6 +2,5 @@ _base_ = [
     '../_base_/models/condnet_r50-d8.py', '../_base_/datasets/ade20k.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
-model = dict(
-    decode_head=dict(num_classes=150),
-    auxiliary_head=dict(num_classes=150))
+model = dict(decode_head=dict(num_classes=150),
+             auxiliary_head=dict(num_classes=150))
